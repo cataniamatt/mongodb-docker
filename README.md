77639499d76787ffb4abbb75189b5018bc1cbd6d
Matthew Catania <matthewcatania@hotmail.com>
2022-04-30 11:33:43 +0200

    Uploaded the MongoDB logo image

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# mongodb-docker
+# MongoDB Step-by-Step
+This is a guide for creating a simple MongoDB database with some collections and documents. In MongoDB, a collection is similar to a table in SQL while documents are similar to rows in tables.
+
+
+Start an interactive shell for the mongodb container using the following command:
+docker exec -it container_name bash
+"-i" tag interactive mode
+"-t" tag
+ 
+On the mongodb container run the following command to access the database
+mongo mongodb://localhost:27017 -u admin -p password
+
---
+++ cars.js
@@ -0,0 +1,86 @@
+car = {
+    "manufacturer": "Mercedes",
+    "model": "W176",
+    "engineSize": "",
+    "variant": "A180d",
+    "trim": "AMG",
+    "year": 2016,
+    "mileage": 77895,
+    "performance": {
+        "power": 109,
+        "torque": 154,
+        "topSpeed": 180,
+        "acceleration": 6.4
+    }
+}
+
+
+cars = [
+    {
+        "manufacturer": "Mercedes",
+        "model": "W176",
+        "engineSize": 1486,
+        "variant": "A180d",
+        "trim": "AMG",
+        "year": 2016,
+        "mileage": 77895,
+        "performance": {
+            "power": 109,
+            "torque": 154,
+            "topSpeed": 180,
+            "acceleration": 10.4
+        },
+        "price": 14000,
+        "colour": "Grey"
+    },
+    {
+        "manufacturer": "Mercedes",
+        "model": "W176",
+        "engineSize": 2000,
+        "variant": "A250d",
+        "trim": "Sport",
+        "year": 2017,
+        "mileage": 45429,
+        "performance": {
+            "power": 184,
+            "torque": 220,
+            "topSpeed": 204,
+            "acceleration": 6.4
+        },
+        "price": 13000,
+        "colour": "Blue"
+    },
+    {
+        "manufacturer": "Volkswagen",
+        "model": "Polo",
+        "engineSize": 1000,
+        "trim": "R-Line",
+        "year": 2019,
+        "mileage": 14690,
+        "performance": {
+            "power": 95,
+            "torque": 110,
+            "topSpeed": 160,
+            "acceleration": 11.4
+        },
+        "price": 17000,
+        "colour": "Orange"
+    },
+    {
+        "manufacturer": "Mercedes",
+        "model": "W176",
+        "engineSize": 4000,
+        "variant": "A45",
+        "trim": "AMG",
+        "year": 2016,
+        "mileage": 21083,
+        "performance": {
+            "power": 376,
+            "torque": 450,
+            "topSpeed": 280,
+            "acceleration": 3.2
+        },
+        "price": 32000,
+        "colour": "White"
+    }
+]
